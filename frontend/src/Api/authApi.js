ddece74b6c1f892ad681e12cf2db2aa8735a922f
Vikash Kumar <103258574+vikashvicky7575@users.noreply.github.com>
2025-07-03 16:06:40 +0530

    Update authApi.js

--- frontend/src/Api/authApi.js
+++ frontend/src/Api/authApi.js
@@ -2,7 +2,7 @@ const BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/auth`
 
 //RegisterAPI
 export const registerAPI = async (payload) => {
-    const res = await fetch(`${BASE_URL}/api/register`, {
+    const res = await fetch(`${BASE_URL}/register`, {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
